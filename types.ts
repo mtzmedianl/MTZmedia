16dce857a66d9b03db7d99e6aa5f3022dee69480
mtzmedianl <mateusz@mtzmedia.nl>
2025-12-05 17:47:57 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,7 +2,7 @@ export interface Project {
   id: number;
   title: string;
   category: string;
-  thumbnail: string;
+  thumbnail: null;
   videoUrl?: string; // URL for the video file
   description: string;
   client?: string;
