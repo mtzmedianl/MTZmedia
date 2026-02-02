3701f12325316c2bfbef784c7df5bb251cd7cf33
mtzmedianl <mateusz@mtzmedia.nl>
2026-02-02 20:35:09 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -104,7 +104,7 @@ const About: React.FC = () => {
             {/* PNG met transparantie */}
             <div className="relative aspect-[4/5] border border-white/10 overflow-hidden rounded-sm">
               <img
-                src="https://i.ibb.co/ymWYGpDC/IMG-6592.png"
+                src="https://i.imgur.com/L3vTtIg.png"
                 alt="Selfie"
                 className="w-full h-full object-cover"
               />
